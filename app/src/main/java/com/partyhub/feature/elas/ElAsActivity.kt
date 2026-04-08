e2fd4522486a1f238a80f7d4b199c8626d31400a
rromeromonzon <rafael.romerom@estudiante.uam.es>
2026-04-08 11:57:30 +0200

    fix(elas): añadir navGraph faltante para solucionar pantalla blanca al iniciar

--- app/src/main/java/com/partyhub/feature/elas/ElAsActivity.kt
+++ app/src/main/java/com/partyhub/feature/elas/ElAsActivity.kt
@@ -2,15 +2,12 @@ package com.partyhub.feature.elas
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.partyhub.databinding.ActivityElAsBinding
+import com.partyhub.R
 
 class ElAsActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityElAsBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityElAsBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_el_as)
     }
 }
--- app/src/main/java/com/partyhub/feature/themind/TheMindActivity.kt
+++ app/src/main/java/com/partyhub/feature/themind/TheMindActivity.kt
@@ -2,15 +2,12 @@ package com.partyhub.feature.themind
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.partyhub.databinding.ActivityTheMindBinding
+import com.partyhub.R
 
 class TheMindActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityTheMindBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityTheMindBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_the_mind)
     }
 }
